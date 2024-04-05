fe4f49dca365db0fdd3da6f12f421a1c2fbb4383
lalalalalala22222222 <165372113+lalalalalala22222222@users.noreply.github.com>
2024-04-05 14:42:22 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,13 @@
-// Получаем все ссылки панели
-навигации
+// Получаем все ссылки панелинавигации
 const navLinks = document.querySelectorAll('.nav-link');
 // Получаем все разделы страницы
 const sections = document.querySelectorAll('.section');
-// Добавляем обработчик события для
-каждой ссылки
+// Добавляем обработчик события длякаждой ссылки
 navLinks.forEach(link => {
  link.addEventListener('click', () => {
- // Получаем идентификатор
-раздела, на который ссылается
-ссылка
+ // Получаем идентификаторраздела, на который ссылаетсяссылка
  const targetId = link.getAttribute('href').substring(1);
- // Отображаем целевой раздел,
-скрывая все остальные
+ // Отображаем целевой раздел,скрывая все остальные
  sections.forEach(section => {
  if (section.id === targetId) {
  section.classList.add('active');
